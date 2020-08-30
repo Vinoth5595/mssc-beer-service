45c04c0d9fab84b77554c84c27a1b6b71c41eed4
Vinoth Manoharan <vinothm.manoharan@gamil.com>
2020-08-30 21:39:06 +0530

    Added validation and error handling. Closes #9

--- src/main/java/com/javageek/web/controller/BeerController.java
+++ src/main/java/com/javageek/web/controller/BeerController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,12 +27,12 @@ public class BeerController {
 	}
 	
 	@PostMapping
-	public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+	public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto) {
 		return new ResponseEntity(HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/{beerId}")
-	public ResponseEntity updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+	public ResponseEntity updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto) {
 		return new ResponseEntity(HttpStatus.NO_CONTENT);
 	}
 
