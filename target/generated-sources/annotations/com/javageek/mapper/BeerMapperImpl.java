00279eefbe05715a134f09c1ed0d1dfefaaa9b64
Vinoth Manoharan <vinothm.manoharan@gamil.com>
2020-09-15 23:10:41 +0530

    updated

--- target/generated-sources/annotations/com/javageek/mapper/BeerMapperImpl.java
+++ target/generated-sources/annotations/com/javageek/mapper/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-09-14T18:44:35+0530",
+    date = "2020-09-15T22:36:36+0530",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
 )
 @Component
